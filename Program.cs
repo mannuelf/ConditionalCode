da9bfe2dd033aecc816d8bd15ecdeb9bb84b8bbf
Mannuel <mannuel.ferreira@gmail.com>
2013-04-15 15:42:43 +0200

    Conditional Code
    C# Conditional Code

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionalCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int theVal = 49;
+
+            if ((theVal == 50) && (theVal == 49))
+            {
+                Console.WriteLine("theVal is 50 or 49");
+            }
+            else if (theVal == 51)
+            {
+                Console.WriteLine("theVal is 51");
+            }
+            else
+            {
+                Console.WriteLine("theVal is something else");
+            }
+            Console.ReadLine();
+        }
+    }
+}
